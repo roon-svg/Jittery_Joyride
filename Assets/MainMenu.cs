b6e80a9a91faa16dccabce3ada353e22929b2806
anshumkumar <anshum1010@hotmail.com>
2023-10-23 13:23:43 +0100

    Version 0.08
    The main menu was set by me. The icons were created by a photo editor app, they were then imported in unity and I created them as clickable buttons. I wrote a script for the play button, if the user presses it, the game starts, For the quit button, I need to add instructions in my script for the code to close the game. Additionally, for the option and about button, I need to add dialogue boxes containing information and further functions in the game.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Playgame()
+    {
+        SceneManager.LoadScene("Level 1");
+        // In the line above, we add the scene of the game, the first level where the game begins.
+
+    }
+
+}
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,3 +20,4 @@ public class Timer : MonoBehaviour
         cTime.Text
     }
 }
+*/
