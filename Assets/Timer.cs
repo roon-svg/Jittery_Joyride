d142afd175ba250f42dbd25cf66561a8e6010073
roon-svg <138370200+roon-svg@users.noreply.github.com>
2023-10-23 15:07:38 +0100

    Version 0.09
    I have added the timer function. The problem with it is that the timer is not showing in the game screen.

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,23 +1,33 @@
-/*
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
 public class Timer : MonoBehaviour
 {
-    public float cTime = 0f;
-    public float startingTime = 60f;
+    public TextMeshProUGUI countdownText;
+
+    public float currentTime = 0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        cTime = startingTime;
+        
     }
 
     // Update is called once per frame
+    // This allows the countdown timer to work.
     void Update()
     {
-        cTime -= 1 * Time.deltaTime;
-        cTime.Text
+        currentTime -= Time.deltaTime;
+        countdownText.text = currentTime.ToString();
+
+        if (currentTime == 0)
+        {
+            //OnApplicationQuit();
+        }
     }
 }
-*/
