dbe07821acecee1c01dcd65538d7bc49c0f3bf9a
roon-svg <138370200+roon-svg@users.noreply.github.com>
2023-10-31 14:21:45 +0000

    Version 0.25

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -32,22 +32,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #region player movement
-        // This allows for the side ways input to be stored
         float horizontalInput = Input.GetAxis("Horizontal");
-        // This allows for the vertical input to be stored.
         float verticalInput = Input.GetAxis("Vertical");
 
-        // This allows the player to go diagonally if there are 2 keys pressed at once.
         Vector2 Direction = new Vector2(horizontalInput, verticalInput);
         float Magnitude = Mathf.Clamp01(Direction.magnitude);
         Direction.Normalize();
 
-        // This code makes the player move.
         transform.Translate(Direction * Speed * Magnitude * Time.deltaTime, Space.World);
-        #endregion
 
-        #region player rotation
         //This allows the player to rotate
         if (Direction != Vector2.zero)
         {
